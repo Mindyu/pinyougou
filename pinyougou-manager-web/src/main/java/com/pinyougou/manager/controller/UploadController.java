c5aa1efe4f6dd1b7772f2d532c24600f104b9c85
Mindyu <704191988@qq.com>
2018-11-30 15:30:36 +0800

    运营商后台广告管理模块

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/UploadController.java
@@ -0,0 +1,41 @@
+package com.pinyougou.manager.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import entity.Result;
+import util.FastDFSClient;
+
+/**
+ * 文件上传
+ * @author YCQ
+ *
+ */
+@RestController
+public class UploadController {
+	
+	@Value("${FILE_SERVER_URL}")
+	private String FILE_SERVER_URL;		// 文件服务器地址 http://ip/
+	
+	@RequestMapping("/upload")
+	public Result upload(MultipartFile file) {
+		
+		String originFileName = file.getOriginalFilename();	// 获取文件名
+		String extName = originFileName.substring(originFileName.lastIndexOf('.')+1);	// 获取扩展名
+		
+		try {
+			FastDFSClient client = new FastDFSClient("classpath:config/fdfs_client.conf");
+			String file_id = client.uploadFile(file.getBytes(), extName);
+			String url = FILE_SERVER_URL + file_id;			// 图片的完整地址
+			return new Result(true, url);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "上传失败");
+		}
+		
+	}
+	
+
+}
